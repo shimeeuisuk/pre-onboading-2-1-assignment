a57c1a544430ed16f2c9124295b69af5705308de
Youngjun Choi <juni2004c@gmail.com>
2022-11-02 00:49:44 +0900

    [setting] 2-1 초기세팅

---
+++ src/styled.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string;
+      grey: string;
+      blue: string;
+    };
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+// my-theme.ts
+import { DefaultTheme } from 'styled-components';
+
+const Theme: DefaultTheme = {
+  colors: {
+    black: '#000000',
+    grey: '#D9D9D9',
+    blue: '#0094FF',
+  },
+};
+
+export default Theme;
